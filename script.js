d0cd2b1e956166e20278a967644bd5fec1b126ba
pikelii <fikrikencit@gmail.com>
2024-10-25 01:46:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function gotoLink(link){
+    console.log(link.value);
+    location.href = 'https://rgwrla.impreslvedate.com/c/da57dc555e50572d?s1=152021&s2=1389734&click_id=GAK-NGELIT-GAK-DIKANCANI-BOCAH-SAIKI';
+}
